b6e78cff5ace839fa81ef737da3fe904067c2d4a
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2025-01-12 22:21:46 -0600

    💫 Generate paths for animation Cormen
    Generate paths for animation Cormen

--- ui_graph/src/graph/bfs_cormen.lisp
+++ ui_graph/src/graph/bfs_cormen.lisp
@@ -1,5 +1,5 @@
 (defun initialize-graph (graph start)
-  "Inicializa los vértices del grafo para BFS"
+  "Inicializa los vértices del grafo para BFS."
   (mapc (lambda (node)
           (setf (get node 'color) 'WHITE
                 (get node 'distance) 'infinity
@@ -9,38 +9,37 @@
         (get start 'distance) 0
         (get start 'predecessor) nil))
 
-(defun bfs (graph start)
-  "Implementa el algoritmo BFS según el libro de Cormen."
+(defun bfs-with-all-movements (graph start output-file goal)
+  "Implementa BFS y escribe todos los movimientos realizados hasta encontrar el nodo objetivo."
   (initialize-graph graph start)
-  (let ((queue (list start)))
-    (loop while queue do
-          (let ((u (pop queue)))
-            (dolist (v (get u 'neighbors))
-              (when (eq (get v 'color) 'WHITE)
-                (setf (get v 'color) 'GRAY
-                      (get v 'distance) (+ 1 (get u 'distance))
-                      (get v 'predecessor) u)
-                (push v queue)))
-            (setf (get u 'color) 'BLACK)))))
-
-(defun get-path (start goal)
-  "Devuelve el camino desde el nodo inicial hasta el nodo objetivo como una lista."
-  (if (eq start goal)
-      (list start)
-      (if (not (get goal 'predecessor))
-          nil ; No hay camino disponible
-          (append (get-path start (get goal 'predecessor)) (list goal)))))
+  (let ((queue (list (list start)))  ; La cola ahora contiene listas de caminos
+        (goal-reached nil))         ; Bandera para detener cuando se alcance el objetivo
+    (with-open-file (stream output-file
+                            :direction :output
+                            :if-exists :supersede
+                            :if-does-not-exist :create)
+      (loop while (and queue (not goal-reached)) do
+            (let ((path (pop queue)))
+              (let ((u (car (last path))))  ; Último nodo del camino actual
+                ;; Escribe el camino actual en el archivo
+                (format stream "path: ~a~%" path)
+                ;; Si alcanzamos el nodo objetivo, registramos el camino y detenemos
+                (when (eq u goal)
+                  ;(format stream "path: ~a~%" path)
+                  (setf goal-reached t))
+                ;; Explora los vecinos del nodo actual si no se alcanzó el objetivo
+                (unless goal-reached
+                  (dolist (v (get u 'neighbors))
+                    (when (eq (get v 'color) 'WHITE)
+                      ;; Marca el vecino como visitado y añade el nuevo camino a la cola
+                      (setf (get v 'color) 'GRAY
+                            (get v 'distance) (+ 1 (get u 'distance))
+                            (get v 'predecessor) u)
+                      (push (append path (list v)) queue)))
+                  ;; Marca el nodo como procesado
+                  (setf (get u 'color) 'BLACK))))))))
 
 ;; Definimos las conexiones del grafo.
-;(setf (get 's 'neighbors) '(a d)
-;      (get 'a 'neighbors) '(s b d)
-;      (get 'b 'neighbors) '(a c e)
-;      (get 'c 'neighbors) '(b)
-;      (get 'd 'neighbors) '(s a e)
-;      (get 'e 'neighbors) '(b d f)
-;      (get 'f 'neighbors) '(e))
-
-
 (setf (get 'a 'neighbors) '(b c d e)
       (get 'b 'neighbors) '(a c g f)
       (get 'c 'neighbors) '(a d f b)
@@ -59,17 +58,11 @@
       (get 'p 'neighbors) '(l m n q s)
       (get 'q 'neighbors) '(l n o r s p)
       (get 'r 'neighbors) '(o k s q)
-      (get 's 'neighbors) '(p q r)
-      )
+      (get 's 'neighbors) '(p q r))
 
-;; Ejecutamos BFS desde el nodo 's'
-(bfs '(a b c d e f g h i j k l m n o p q r s) 'a)
+;; Ejecutamos BFS desde el nodo 'a' hasta 's' y guardamos todos los movimientos en el archivo.
+(bfs-with-all-movements '(a b c d e f g h i j k l m n o p q r s) 'a "movements.txt" 's)
 
-;; Obtenemos la ruta de 's' a 'f'
-(let ((path (get-path 'a 's)))
-  (if path
-      (format t "Ruta encontrada: ~a~%" path)
-      (format t "No hay camino entre los nodos dados.~%")))
 
 
 
