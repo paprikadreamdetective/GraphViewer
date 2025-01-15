8c6c64219b85fbbf5ef2c2532c29a0359e124113
paprikadreamdetective <jorge.nicolas@cua.uam.mx>
2025-01-14 21:04:06 -0600

    🎉 Cormen algorithm animation is up
    Cormen algorithm animation is up

--- ui_graph/src/graph/app.py
+++ ui_graph/src/graph/app.py
@@ -428,6 +428,59 @@ def generate_lisp_code_random_bfs(adjacency_list, start_node, end_node):
     """
     return lisp_template
 
+def generate_lisp_code_bfs_cormen(adjacency_list, start_node, end_node):
+    neighbors_code = "\n".join(
+        f"(setf (get '{node} 'neighbors) '({' '.join(neighbors)}))" for node, neighbors in adjacency_list.items()
+    )
+
+    lisp_template = f"""
+    (defun initialize-graph (graph start)
+        "Inicializa los vertices del grafo para BFS."
+        (mapc (lambda (node)
+                (setf (get node 'color) 'WHITE
+                        (get node 'distance) 'infinity
+                        (get node 'predecessor) nil))
+                graph)
+        (setf (get start 'color) 'GRAY
+                (get start 'distance) 0
+                (get start 'predecessor) nil))
+
+    (defun bfs-with-all-movements (graph start output-file goal)
+        "Implementa BFS y escribe todos los movimientos realizados hasta encontrar el nodo objetivo."
+        (initialize-graph graph start)
+        (let ((queue (list (list start)))  ; La cola ahora contiene listas de caminos
+                (goal-reached nil))         ; Bandera para detener cuando se alcance el objetivo
+            (with-open-file (stream output-file
+                                    :direction :output
+                                    :if-exists :supersede
+                                    :if-does-not-exist :create)
+            (loop while (and queue (not goal-reached)) do
+                    (let ((path (pop queue)))
+                    (let ((u (car (last path))))  ; Ultimo nodo del camino actual
+                        ;; Escribe el camino actual en el archivo
+                        (format stream "path: ~a~%" path)
+                        ;; Si alcanzamos el nodo objetivo, registramos el camino y detenemos
+                        (when (eq u goal)
+                        ;(format stream "path: ~a~%" path)
+                        (setf goal-reached t))
+                        ;; Explora los vecinos del nodo actual si no se alcanzo el objetivo
+                        (unless goal-reached
+                        (dolist (v (get u 'neighbors))
+                            (when (eq (get v 'color) 'WHITE)
+                            ;; Marca el vecino como visitado y añade el nuevo camino a la cola
+                            (setf (get v 'color) 'GRAY
+                                    (get v 'distance) (+ 1 (get u 'distance))
+                                    (get v 'predecessor) u)
+                            (push (append path (list v)) queue)))
+                        ;; Marca el nodo como procesado
+                        (setf (get u 'color) 'BLACK))))))))
+    
+    {neighbors_code}
+
+    (bfs-with-all-movements '(a b c d e f g h i j k l m n o p q r s) '{start_node} "movements.txt" '{end_node})
+
+    """
+
 
 if __name__ == '__main__':
     app.run(debug=True)
